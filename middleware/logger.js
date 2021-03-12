a19e05066d00a74b2c12321ff91d59a14054a0d2
apaardahal <cullaapar@gmail.com>
2021-03-13 00:04:38 +0545

    logger implemented

---
+++ middleware/logger.js
@@ -0,0 +1,7 @@
+// Description     Logs requests to console
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.protocal}://${req.get('host')} ${req.originalUrl}`)
+    next();
+};
+
+module.exports = logger;
