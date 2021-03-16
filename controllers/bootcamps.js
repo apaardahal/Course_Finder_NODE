b44805224619ef09ba7983b303954548a022bea3
apaardahal <cullaapar@gmail.com>
2021-03-16 23:25:56 +0545

    CRUD for courses done

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,49 +1,100 @@
+const Bootcamp = require('../models/Bootcamp');
+
 // @description     Get all bootcamps
 // @Route           GET /api/v1/bootcamps
 // @access          Public
-exports.getBootcamps = (req, res, next) => {
-    res.status(200).json({
-        status: true,
-        msg: 'All bootcamps'
-    })
+exports.getBootcamps = async(req, res, next) => {
+    try {
+        const bootcamps = await Bootcamp.find();
+
+        res.status(200).json({
+            status: true,
+            count: bootcamps.length,
+            data: bootcamps
+        })
+    } catch (err) {
+        res.status(400).json({ success: false })
+    }
+
 }
 
 // @description     Get single bootcamps
 // @Route           GET /api/v1/bootcamps/:id
 // @access          Public
-exports.getBootcamp = (req, res, next) => {
-    res.status(200).json({
-        status: true,
-        msg: 'Single bootcamps'
-    })
+exports.getBootcamp = async(req, res, next) => {
+    try {
+        const bootcamp = await Bootcamp.findById(req.params.id);
+        if (!bootcamp) {
+            return res.status(400).json({ success: false })
+        }
+        res.status(200).json({
+            success: true,
+            data: bootcamp
+        })
+    } catch (err) {
+        res.status(400).json({ success: false })
+    }
+
 }
 
-// @description     Create bootcamps
+// @description     Create a new bootcamp
 // @Route           POST /api/v1/bootcamps
 // @access          Private
-exports.createBootcamp = (req, res, next) => {
-    res.status(200).json({
-        status: true,
-        msg: 'Create bootcamp'
-    })
+exports.createBootcamp = async(req, res, next) => {
+    try {
+        const bootcamp = await Bootcamp.create(req.body);
+        res.status(201).json({
+            success: true,
+            data: bootcamp
+        })
+    } catch (err) {
+        res.status(400).json({
+            success: false
+        })
+    }
+
 }
 
 // @description     Update bootcamp
 // @Route           PUT /api/v1/bootcamps/:id
 // @access          Private
-exports.updateBootcamp = (req, res, next) => {
-    res.status(200).json({
-        status: true,
-        msg: 'Update bootcamp'
-    })
+exports.updateBootcamp = async(req, res, next) => {
+    try {
+        const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
+
+        if (!bootcamp) {
+            return res.status(400).json({ success: false });
+        }
+
+        res.status(200).json({
+            success: true,
+            data: bootcamp
+        })
+    } catch (err) {
+        res.status(400).json({ success: false })
+    }
+
 }
 
 // @description     Delete bootcamp
 // @Route           DELETE /api/v1/bootcamps/:id
 // @access          Private
-exports.deleteBootcamp = (req, res, next) => {
-    res.status(200).json({
-        status: true,
-        msg: 'Delete bootcamp'
-    })
+exports.deleteBootcamp = async(req, res, next) => {
+    try {
+        const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+
+        if (!bootcamp) {
+            return res.status(400).json({ success: false });
+        }
+
+        res.status(200).json({
+            success: true,
+            data: {}
+        })
+    } catch (err) {
+        res.status(400).json({ success: false })
+    }
 }
---
+++ models/Bootcamp.js
@@ -0,0 +1,103 @@
+const mongoose = require('mongoose');
+
+const BootcampSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please add a name'],
+        unique: true,
+        trim: true,
+        maxlength: [50, 'Names can not be more than 50 characters']
+    },
+    slug: String,
+    description: {
+        type: String,
+        required: [true, 'Please add a description'],
+        maxlength: [500, 'Description can not be more than 500 characters']
+    },
+    website: {
+        type: String,
+        match: [
+            /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/,
+            'Please add a valid URL with HTTP or HTTPS'
+        ]
+    },
+    phone: {
+        type: String,
+        maxlength: [20, 'Phone number can not be longer than 20 characters']
+    },
+    email: {
+        type: String,
+        match: [
+            /^[\w\-\.\+]+\@[a-zA-Z0-9\.\-]+\.[a-zA-z0-9]{2,5}$/,
+            'Please add a valid email'
+        ]
+    },
+    address: {
+        type: String,
+        required: [true, 'Please add an address']
+    },
+    location: {
+        //GeoJSON Point
+        type: {
+            type: String,
+            enum: ['Point'],
+            required: true,
+        },
+        coordinates: {
+            type: [Number],
+            required: true,
+            index: '2dsphere'
+        },
+        formattedAddress: String,
+        street: String,
+        city: String,
+        state: String,
+        zipCode: String,
+        Country: String
+    },
+    careers: {
+        // Array of Strings
+        type: [String],
+        required: true,
+        enum: [
+            'Web Developent',
+            'Mobile Development',
+            'UI/UX',
+            'Data Science',
+            'Business',
+            'Other'
+        ]
+    },
+    averageRating: {
+        type: Number,
+        min: [1, 'Rating must be atleast 1'],
+        max: [10, 'Rating can not be more than 10']
+    },
+    averageRating: Number,
+    photo: {
+        type: String,
+        default: 'no-photo.jpg'
+    },
+    housing: {
+        type: Boolean,
+        default: false
+    },
+    jobAssistance: {
+        type: Boolean,
+        default: false
+    },
+    jobGuarantee: {
+        type: Boolean,
+        default: false
+    },
+    acceptGi: {
+        type: Boolean,
+        default: false
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('Bootcamp', BootcampSchema);
